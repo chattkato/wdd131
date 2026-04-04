0018ca2f73d3aa81cee91fa47a3197aafc9503f7
chattkato <ckato@byupathway.edu>
2026-04-04 10:29:51 +0100

    aba temple commit

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -12,7 +12,7 @@ const temples = [
 
   // Added temples
   { name: "Accra Ghana Temple", location: "Ghana", dedicated: "2004-01-11", area: 17500, imageUrl: "https://churchofjesuschristtemples.org/accra-ghana-temple/photographs/#Gallery-5.jpg" },
-  { name: "Aba Nigeria Temple", location: "Nigeria", dedicated: "2005-08-07", area: 11500, imageUrl: "https://churchofjesuschristtemples.org/aba-nigeria-temple/photographs/#Official-1.jpg" },
+  { name: "Aba Nigeria Temple", location: "Nigeria", dedicated: "2005-08-07", area: 11500, imageUrl: "https://churchofjesuschristtemples.org/temples/aba-nigeria-temple/photographs/#Official-1.jpg" },
   { name: "Rome Italy Temple", location: "Italy", dedicated: "2019-03-10", area: 41000, imageUrl: "https://churchofjesuschristtemples.org/rome-italy-temple/photographs/#Gallery-2.jpg" }
 ];
 
